8635f31d9fc65eab3e6321619211679152d9038f
ivoryblakk <george.ingram1@outlook.com>
2017-10-03 14:30:03 +0200

    solved

---
+++ callbacks/cb0.js
@@ -0,0 +1,23 @@
+// My code
+function devSide(err, result) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(result);
+  }
+}
+//----------------------------------------------------------------
+function adder(number1, number2, cb) {
+  var result = 0000;
+  var err = null;
+  if (typeof number1 == 'number' && typeof number2 == 'number') {
+    result = number1 + number2;
+  } else {
+    err = 'arguments arent numbers';
+  }
+  cb(err, result);
+}
+
+adder(5, 4, devSide); // -> 9
+
+adder('tim', 4, devSide); // -> arguments arent numbers
