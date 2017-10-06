4b3391438e8e42d03f59094b6facef1b5ad537ea
ivoryblakk <george.ingram1@outlook.com>
2017-10-06 12:29:13 +0200

    anObject

--- objects/anObject.js
+++ objects/anObject.js
@@ -1,26 +1,23 @@
+var anObject = {
+  string1: 'initial value',
+  count: 1,
+  printString: function(number) {
+    var keyname = 'string' + number;
+    console.log(this[keyname]);
+  },
+  modifyString: function(string, number) {
+    var keyname = 'string' + number;
+    this[keyname] = string;
+  },
+  addNewProperty: function(newProperty) {
+    this.count++;
+    var keyname = 'string' + this.count;
+    this[keyname] = newProperty;
+  }
+};
 
-/*
-make this dream a reality
-anOjbect: object
-	properties: 1, initially at least
-		string1: string
-			initialized: 'initial value'
-	methods: 3
-		printString: function
-			args: 1
-				whichToPrint: number indicating which string property to print
-			return: undefined
-			behavior: prints the stringX property
-		modifyString: funciton
-			args: 2
-				newString: new substitute string
-				whichOne: number indicating which property's value to replace
-			return: undefined
-			behavior: replaces stringX's value with newString
-		addNewProperty: function
-			args: 1
-				newProperty: string to add as new property
-			return: undefined
-			behavior: adds a new property -> key=stringX, value=newProperty
-		deleteProperty: write me and my specs
-*/
+anObject.printString(1);
+anObject.modifyString('test string', 1);
+anObject.printString(1);
+anObject.addNewProperty('whasssuupppp');
+anObject.printString(2);
